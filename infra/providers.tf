f316900554ba252c1fc83a5cd3664a6b404210ce
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-03-05 18:02:29 +0200

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -12,7 +12,7 @@ terraform {
 
 provider "aws" {
     region = "us-east-1"
-    profile = "minio"
+    profile = "localstack"
     shared_credentials_file = "/drone/src/.aws/credentials"
     skip_credentials_validation = true
     skip_metadata_api_check = true
