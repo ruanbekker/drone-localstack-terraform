7476a66f433667da8ee9f96b58b6cdf94af2726e
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-03-05 17:53:46 +0200

    Create providers.tf

---
+++ infra/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
+    random = {
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    profile = "minio"
+    shared_credentials_file = "/drone/src/.aws/credentials"
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    skip_requesting_account_id = true
+    s3_force_path_style = true
+    endpoints {
+        s3  = "http://localstack:4568"
+        ec2 = "http://localstack:4568"
+        ssm = "http://localstack:4568"
+    }
+}
+
+provider "random" {}
