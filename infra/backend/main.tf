daf753b5eba159f392ad076c19947a42ef83d436
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-03-05 18:02:42 +0200

    Update main.tf

--- infra/backend/main.tf
+++ infra/backend/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     region = "us-east-1"
-    profile = "minio"
+    profile = "localstack"
     shared_credentials_file = "/drone/src/.aws/credentials"
     skip_credentials_validation = true
     skip_metadata_api_check = true
